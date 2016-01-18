ccce847e2ca1aa7a0d62b1f81e9bcf175b7a397b
Eric Liu <eric.liu.developer@gmail.com>
2016-01-18 19:44:20 +1100

    improves mvp interfaces

--- app/src/main/java/com/ericliudeveloper/weatherforecast/framework/UpdatableView.java
+++ app/src/main/java/com/ericliudeveloper/weatherforecast/framework/UpdatableView.java
@@ -1,7 +1,5 @@
 package com.ericliudeveloper.weatherforecast.framework;
 
-import android.os.Bundle;
-
 /**
  * Created by Eric Liu on 12/01/2016.
  */
@@ -12,18 +10,4 @@ public interface UpdatableView<M> {
     void setPresenter(Presenter presenter);
 
 
-    /**
-     * A listener for events fired off by a {@link Model}
-     */
-    interface UserActionListener {
-
-        /**
-         * Called when the user has performed an {@code action}, with data to be passed
-         * as a {@link Bundle} in {@code args}.
-         * <p/>
-         * Add the constants used to store values in the bundle to the Model implementation class
-         * as final static protected strings.
-         */
-
-    }
 }
